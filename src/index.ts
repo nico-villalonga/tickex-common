d3eadb1416a0a7de8d94ac06aebb83b94440c56d
nico-villalonga <nicovillalonga90@gmail.com>
2021-08-13 16:42:14 -0300

    added ticket created & updated events

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 // Errors
-export * from "./errors/bad-request"
-export * from "./errors/custom-error"
-export * from "./errors/database-connection"
-export * from "./errors/not-found"
-export * from "./errors/request-validation"
-export * from "./errors/unauthorized"
+export * from "./errors/bad-request";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection";
+export * from "./errors/not-found";
+export * from "./errors/request-validation";
+export * from "./errors/unauthorized";
 
 // Middlewares
-export * from "./middlewares/current-user"
-export * from "./middlewares/error-handler"
-export * from "./middlewares/require-auth"
-export * from "./middlewares/validate-request"
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
+
+// Events
+export * from "./events/base-listener";
+export * from "./events/base-publisher";
+export * from "./events/subjects";
+export * from "./events/ticket-created-event";
+export * from "./events/ticket-updated-event";
